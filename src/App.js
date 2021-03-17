743a3ab7a0c76f074bce0f3ac1a6daceeb01ce21
locdt2 <locdtptit@gmail.com>
2021-03-17 11:54:10 +0700

    update image

--- src/App.js
+++ src/App.js
@@ -109,9 +109,11 @@ function App() {
             <div className="col-12">
 
               {/*  Decoration */}
-              {/* <div className="position-absolute top-center top-md-left text-warning mt-n8 ml-md-n8">
-                  <svg width="185" height="186" viewBox="0 0 185 186" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="2" cy="2" r="2" fill="currentColor" /><circle cx="22" cy="2" r="2" fill="currentColor" /><circle cx="42" cy="2" r="2" fill="currentColor" /><circle cx="62" cy="2" r="2" fill="currentColor" /><circle cx="82" cy="2" r="2" fill="currentColor" /><circle cx="102" cy="2" r="2" fill="currentColor" /><circle cx="122" cy="2" r="2" fill="currentColor" /><circle cx="142" cy="2" r="2" fill="currentColor" /><circle cx="162" cy="2" r="2" fill="currentColor" /><circle cx="182" cy="2" r="2" fill="currentColor" /><circle cx="2" cy="22" r="2" fill="currentColor" /><circle cx="22" cy="22" r="2" fill="currentColor" /><circle cx="42" cy="22" r="2" fill="currentColor" /><circle cx="62" cy="22" r="2" fill="currentColor" /><circle cx="82" cy="22" r="2" fill="currentColor" /><circle cx="102" cy="22" r="2" fill="currentColor" /><circle cx="122" cy="22" r="2" fill="currentColor" /><circle cx="142" cy="22" r="2" fill="currentColor" /><circle cx="162" cy="22" r="2" fill="currentColor" /><circle cx="182" cy="22" r="2" fill="currentColor" /><circle cx="2" cy="42" r="2" fill="currentColor" /><circle cx="22" cy="42" r="2" fill="currentColor" /><circle cx="42" cy="42" r="2" fill="currentColor" /><circle cx="62" cy="42" r="2" fill="currentColor" /><circle cx="82" cy="42" r="2" fill="currentColor" /><circle cx="102" cy="42" r="2" fill="currentColor" /><circle cx="122" cy="42" r="2" fill="currentColor" /><circle cx="142" cy="42" r="2" fill="currentColor" /><circle cx="162" cy="42" r="2" fill="currentColor" /><circle cx="182" cy="42" r="2" fill="currentColor" /><circle cx="2" cy="62" r="2" fill="currentColor" /><circle cx="22" cy="62" r="2" fill="currentColor" /><circle cx="42" cy="62" r="2" fill="currentColor" /><circle cx="62" cy="62" r="2" fill="currentColor" /><circle cx="82" cy="62" r="2" fill="currentColor" /><circle cx="102" cy="62" r="2" fill="currentColor" /><circle cx="122" cy="62" r="2" fill="currentColor" /><circle cx="142" cy="62" r="2" fill="currentColor" /><circle cx="162" cy="62" r="2" fill="currentColor" /><circle cx="182" cy="62" r="2" fill="currentColor" /><circle cx="2" cy="82" r="2" fill="currentColor" /><circle cx="22" cy="82" r="2" fill="currentColor" /><circle cx="42" cy="82" r="2" fill="currentColor" /><circle cx="62" cy="82" r="2" fill="currentColor" /><circle cx="82" cy="82" r="2" fill="currentColor" /><circle cx="102" cy="82" r="2" fill="currentColor" /><circle cx="122" cy="82" r="2" fill="currentColor" /><circle cx="142" cy="82" r="2" fill="currentColor" /><circle cx="162" cy="82" r="2" fill="currentColor" /><circle cx="182" cy="82" r="2" fill="currentColor" /><circle cx="2" cy="102" r="2" fill="currentColor" /><circle cx="22" cy="102" r="2" fill="currentColor" /><circle cx="42" cy="102" r="2" fill="currentColor" /><circle cx="62" cy="102" r="2" fill="currentColor" /><circle cx="82" cy="102" r="2" fill="currentColor" /><circle cx="102" cy="102" r="2" fill="currentColor" /><circle cx="122" cy="102" r="2" fill="currentColor" /><circle cx="142" cy="102" r="2" fill="currentColor" /><circle cx="162" cy="102" r="2" fill="currentColor" /><circle cx="182" cy="102" r="2" fill="currentColor" /><circle cx="2" cy="122" r="2" fill="currentColor" /><circle cx="22" cy="122" r="2" fill="currentColor" /><circle cx="42" cy="122" r="2" fill="currentColor" /><circle cx="62" cy="122" r="2" fill="currentColor" /><circle cx="82" cy="122" r="2" fill="currentColor" /><circle cx="102" cy="122" r="2" fill="currentColor" /><circle cx="122" cy="122" r="2" fill="currentColor" /><circle cx="142" cy="122" r="2" fill="currentColor" /><circle cx="162" cy="122" r="2" fill="currentColor" /><circle cx="182" cy="122" r="2" fill="currentColor" /><circle cx="2" cy="142" r="2" fill="currentColor" /><circle cx="22" cy="142" r="2" fill="currentColor" /><circle cx="42" cy="142" r="2" fill="currentColor" /><circle cx="62" cy="142" r="2" fill="currentColor" /><circle cx="82" cy="142" r="2" fill="currentColor" /><circle cx="102" cy="142" r="2" fill="currentColor" /><circle cx="122" cy="142" r="2" fill="currentColor" /><circle cx="142" cy="142" r="2" fill="currentColor" /><circle cx="162" cy="142" r="2" fill="currentColor" /><circle cx="182" cy="142" r="2" fill="currentColor" /><circle cx="2" cy="162" r="2" fill="currentColor" /><circle cx="22" cy="162" r="2" fill="currentColor" /><circle cx="42" cy="162" r="2" fill="currentColor" /><circle cx="62" cy="162" r="2" fill="currentColor" /><circle cx="82" cy="162" r="2" fill="currentColor" /><circle cx="102" cy="162" r="2" fill="currentColor" /><circle cx="122" cy="162" r="2" fill="currentColor" /><circle cx="142" cy="162" r="2" fill="currentColor" /><circle cx="162" cy="162" r="2" fill="currentColor" /><circle cx="182" cy="162" r="2" fill="currentColor" /><circle cx="2" cy="182" r="2" fill="currentColor" /><circle cx="22" cy="182" r="2" fill="currentColor" /><circle cx="42" cy="182" r="2" fill="currentColor" /><circle cx="62" cy="182" r="2" fill="currentColor" /><circle cx="82" cy="182" r="2" fill="currentColor" /><circle cx="102" cy="182" r="2" fill="currentColor" /><circle cx="122" cy="182" r="2" fill="currentColor" /><circle cx="142" cy="182" r="2" fill="currentColor" /><circle cx="162" cy="182" r="2" fill="currentColor" /><circle cx="182" cy="182" r="2" fill="currentColor" /></svg>
-                </div> */}
+              <div className="position-absolute top-center top-md-left text-warning mt-n8 ml-md-n8">
+                <svg width="185" height="186" viewBox="0 0 185 186" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="2" cy="2" r="2" fill="currentColor" /><circle cx="22" cy="2" r="2" fill="currentColor" /><circle cx="42" cy="2" r="2" fill="currentColor" />
+                  <circle cx="62" cy="2" r="2" fill="currentColor" /><circle cx="82" cy="2" r="2" fill="currentColor" /><circle cx="102" cy="2" r="2" fill="currentColor" /><circle cx="122" cy="2" r="2" fill="currentColor" /><circle cx="142" cy="2" r="2" fill="currentColor" /><circle cx="162" cy="2" r="2" fill="currentColor" /><circle cx="182" cy="2" r="2" fill="currentColor" /><circle cx="2" cy="22" r="2" fill="currentColor" /><circle cx="22" cy="22" r="2" fill="currentColor" /><circle cx="42" cy="22" r="2" fill="currentColor" /><circle cx="62" cy="22" r="2" fill="currentColor" /><circle cx="82" cy="22" r="2" fill="currentColor" /><circle cx="102" cy="22" r="2" fill="currentColor" /><circle cx="122" cy="22" r="2" fill="currentColor" /><circle cx="142" cy="22" r="2" fill="currentColor" /><circle cx="162" cy="22" r="2" fill="currentColor" /><circle cx="182" cy="22" r="2" fill="currentColor" /><circle cx="2" cy="42" r="2" fill="currentColor" /><circle cx="22" cy="42" r="2" fill="currentColor" /><circle cx="42" cy="42" r="2" fill="currentColor" /><circle cx="62" cy="42" r="2" fill="currentColor" /><circle cx="82" cy="42" r="2" fill="currentColor" /><circle cx="102" cy="42" r="2" fill="currentColor" /><circle cx="122" cy="42" r="2" fill="currentColor" /><circle cx="142" cy="42" r="2" fill="currentColor" /><circle cx="162" cy="42" r="2" fill="currentColor" /><circle cx="182" cy="42" r="2" fill="currentColor" /><circle cx="2" cy="62" r="2" fill="currentColor" /><circle cx="22" cy="62" r="2" fill="currentColor" /><circle cx="42" cy="62" r="2" fill="currentColor" /><circle cx="62" cy="62" r="2" fill="currentColor" /><circle cx="82" cy="62" r="2" fill="currentColor" /><circle cx="102" cy="62" r="2" fill="currentColor" /><circle cx="122" cy="62" r="2" fill="currentColor" /><circle cx="142" cy="62" r="2" fill="currentColor" /><circle cx="162" cy="62" r="2" fill="currentColor" /><circle cx="182" cy="62" r="2" fill="currentColor" /><circle cx="2" cy="82" r="2" fill="currentColor" /><circle cx="22" cy="82" r="2" fill="currentColor" /><circle cx="42" cy="82" r="2" fill="currentColor" /><circle cx="62" cy="82" r="2" fill="currentColor" /><circle cx="82" cy="82" r="2" fill="currentColor" /><circle cx="102" cy="82" r="2" fill="currentColor" /><circle cx="122" cy="82" r="2" fill="currentColor" /><circle cx="142" cy="82" r="2" fill="currentColor" /><circle cx="162" cy="82" r="2" fill="currentColor" /><circle cx="182" cy="82" r="2" fill="currentColor" /><circle cx="2" cy="102" r="2" fill="currentColor" /><circle cx="22" cy="102" r="2" fill="currentColor" /><circle cx="42" cy="102" r="2" fill="currentColor" /><circle cx="62" cy="102" r="2" fill="currentColor" /><circle cx="82" cy="102" r="2" fill="currentColor" /><circle cx="102" cy="102" r="2" fill="currentColor" /><circle cx="122" cy="102" r="2" fill="currentColor" /><circle cx="142" cy="102" r="2" fill="currentColor" /><circle cx="162" cy="102" r="2" fill="currentColor" /><circle cx="182" cy="102" r="2" fill="currentColor" /><circle cx="2" cy="122" r="2" fill="currentColor" /><circle cx="22" cy="122" r="2" fill="currentColor" /><circle cx="42" cy="122" r="2" fill="currentColor" /><circle cx="62" cy="122" r="2" fill="currentColor" /><circle cx="82" cy="122" r="2" fill="currentColor" /><circle cx="102" cy="122" r="2" fill="currentColor" /><circle cx="122" cy="122" r="2" fill="currentColor" /><circle cx="142" cy="122" r="2" fill="currentColor" /><circle cx="162" cy="122" r="2" fill="currentColor" /><circle cx="182" cy="122" r="2" fill="currentColor" /><circle cx="2" cy="142" r="2" fill="currentColor" /><circle cx="22" cy="142" r="2" fill="currentColor" /><circle cx="42" cy="142" r="2" fill="currentColor" /><circle cx="62" cy="142" r="2" fill="currentColor" /><circle cx="82" cy="142" r="2" fill="currentColor" /><circle cx="102" cy="142" r="2" fill="currentColor" /><circle cx="122" cy="142" r="2" fill="currentColor" /><circle cx="142" cy="142" r="2" fill="currentColor" /><circle cx="162" cy="142" r="2" fill="currentColor" /><circle cx="182" cy="142" r="2" fill="currentColor" /><circle cx="2" cy="162" r="2" fill="currentColor" /><circle cx="22" cy="162" r="2" fill="currentColor" /><circle cx="42" cy="162" r="2" fill="currentColor" /><circle cx="62" cy="162" r="2" fill="currentColor" /><circle cx="82" cy="162" r="2" fill="currentColor" /><circle cx="102" cy="162" r="2" fill="currentColor" /><circle cx="122" cy="162" r="2" fill="currentColor" /><circle cx="142" cy="162" r="2" fill="currentColor" /><circle cx="162" cy="162" r="2" fill="currentColor" /><circle cx="182" cy="162" r="2" fill="currentColor" /><circle cx="2" cy="182" r="2" fill="currentColor" /><circle cx="22" cy="182" r="2" fill="currentColor" /><circle cx="42" cy="182" r="2" fill="currentColor" /><circle cx="62" cy="182" r="2" fill="currentColor" /><circle cx="82" cy="182" r="2" fill="currentColor" /><circle cx="102" cy="182" r="2" fill="currentColor" /><circle cx="122" cy="182" r="2" fill="currentColor" /><circle cx="142" cy="182" r="2" fill="currentColor" /><circle cx="162" cy="182" r="2" fill="currentColor" /><circle cx="182" cy="182" r="2" fill="currentColor" /></svg>
+                </div>
 
               {/*  Stack */}
               <div className="card-stack">
@@ -142,9 +144,9 @@ function App() {
                           </p>
 
                           {/*  Link */}
-                          <a className="h6 text-uppercase text-decoration-none mt-auto" 
-                          target="_blank"
-                          href="/scroll-animation">
+                          <a className="h6 text-uppercase text-decoration-none mt-auto"
+                            target="_blank"
+                            href="/scroll-animation">
                             View <i className="fe fe-arrow-right"></i>
                           </a>
 
@@ -179,12 +181,48 @@ function App() {
                           <p>This project uses only css, no javascript.</p>
 
                           {/*  Link */}
-                          <a className="h6 text-uppercase text-decoration-none mt-auto" 
-                          target="_blank"
-                          href="/background-animation">
+                          <a className="h6 text-uppercase text-decoration-none mt-auto"
+                            target="_blank"
+                            href="/background-animation">
                             View <i className="fe fe-arrow-right"></i>
                           </a>
-                           
+
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/*  Card */}
+                <div className="card-stack-item mb-9">
+                  <div className="card card-lg rounded overflow-hidden">
+                    <div className="row no-gutters">
+                      <div className="col-md order-md-1 d-flex align-items-end bg-primary">
+
+                        {/*  Image */}
+                        <a target="_blank"
+                          href="/parallax-text-scrolling">
+                          <img className="card-img-top" src="/p-parallax-text-scrolling.png" alt="p-parallax-text-scrolling" />
+                        </a>
+
+                      </div>
+                      <div className="col-md-5 order-md-0 d-flex align-items-center bg-white">
+                        <div className="card-body text-center">
+
+                          {/*  Heading */}
+                          <h1 className="font-family-sans-serif mb-4 mt-auto">
+                            Text scrolling
+                          </h1>
+
+                          {/*  Text */}
+                          <p>This project using css and a little javascript.</p>
+
+                          {/*  Link */}
+                          <a className="h6 text-uppercase text-decoration-none mt-auto"
+                            target="_blank"
+                            href="/parallax-text-scrolling">
+                            View <i className="fe fe-arrow-right"></i>
+                          </a>
+
                         </div>
                       </div>
                     </div>
