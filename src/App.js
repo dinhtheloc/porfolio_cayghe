fc3c2026b0df39fe83c8ef5dcf5d2c3e987143fa
locdt2 <locdtptit@gmail.com>
2021-09-22 09:38:27 +0700

    update content

--- src/App.js
+++ src/App.js
@@ -55,18 +55,16 @@ function App() {
             </a>
 
             <h1 className="title">
-              My name's <a href="https://www.instagram.com/locdt94/" target="_blank" rel="noreferrer">Dinh The Loc</a>. My friends call me cayghe.
-              I allways strives to build immersive and beautiful web applications through carefully crafted code and user-centric design.
+              My name's <a href="https://www.instagram.com/locdt94/" target="_blank" rel="noreferrer">Dinh The Loc</a>.
+              I always strive to build immersive and beautiful web applications through carefully crafted code and user-centric design.
             </h1>
             <div className="body">
-              <p>I have had for 4 years experiences in Frontend developer. I have worked for <a href="https://www.vng.com.vn/" target="_blank" rel="noreferrer">
-                VNG Corporation</a> since 2017 to present.</p>
-              <p>My strong points are <a href="https://angular.io/" target="_blank" rel="noreferrer">Angular</a> and <a href="https://reactjs.org/" target="_blank" rel="noreferrer">React</a> framework.
+              <p>I have had 4 years of experience as a Front-end developer. I have worked for <a href="https://www.vng.com.vn/" target="_blank" rel="noreferrer">
+                VNG Corporation</a> from 2017 to the present.</p>
+              <p>My strong points are <a href="https://angular.io/" target="_blank" rel="noreferrer">Angular</a> and <a href="https://reactjs.org/" target="_blank" rel="noreferrer">React</a> frameworks.
               If you want to read my resume details. <a href="/resume.pdf" target="_blank" rel="noreferrer">Click here</a></p>
 
-              <p>Three words to describe myself are "Avtive", "Friendly" and "Comic".
-              My hobby is playing games. However, I’m not addicted to the game at all.
-              I only play games when I’ve done my work. Besides, I also like football and music.</p>
+              <p>Three words to describe myself are "Active", "Friendly" and "Comic". My hobby is playing games. However, I’m not addicted to the game at all. I only play games when I’ve done my work. Besides, I also like football and music.</p>
             </div>
           </div>
         </div>
